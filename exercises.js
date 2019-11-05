7cf9dbdffe4624a06c47d93ba8e4a65a0f1c478b
ShontraePasley <shontraep@gmail.com>
2019-11-04 19:45:57 -0600

    changes

--- exercises.js
+++ exercises.js
@@ -1,14 +1,17 @@
 // 1 create an alert that says "hello world"
+console.log("Hello" "+" "World");
 
 // 2 create a prompt which asks for your favorite number
-    // 2a display the response in the console with "user's favorite nunber is: (display input)"
 
+console.log("What is your favorite number?");
+// 2a display the response in the console with "user's favorite nunber is: (display input)"
+
+console.log("6");
 // 3 create a variable which selects the h1
     // 3a add inner text to the header selector
-    var header = document.querySelector("h1");
-    header.innerHTML = "hi heading"
+    
     // 3b add a border around the h1 that is 1px, solid, and black. use "cssText" to add this
-        header.style.cssText = "border: 1px solid black";
+ 
 
 // 4 console.log a variable tht holds an array of the alphabet
     // 4a use "slice" to remove the vowels, including "y"
